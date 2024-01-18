9d7c268eed72ceb05d6367ae37ced3f4644d0f3a
DuRuofu <duruofu@qq.com>
2024-01-18 22:32:47 +0800

    feat(all): completes the underlying dependency configuration

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,23 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+// 导入Pinia
 app.use(createPinia())
+
+// 导入路由
 app.use(router)
+
+// 导入ElementPlus
+app.use(ElementPlus, {
+  locale: zhCn
+})
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,22 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    // 端口号
+    port: 8080,
+    // 监听所有地址
+    host: '0.0.0.0',
+    // 服务启动时是否自动打开浏览器
+    open: true,
+    // 允许跨域
+    cors: true,
+    // 自定义代理规则
+    proxy: {}
   }
 })
